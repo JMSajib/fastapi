3fd8d67b1efabd200802dd5713644d4b37b9c785
jahidul <jahidul@nexgencloud.com>
2024-08-15 13:40:33 +0600

    Added postgresql connection and added books table

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from src.books.routes import book_router
+from contextlib import asynccontextmanager
+from src.db.main import initdb
+
+# lifespan event
+@asynccontextmanager
+async def life_span(app: FastAPI):
+    await initdb()
+    yield
+    print("Server is stopping")
+
 
 version = 'v1'
 
@@ -7,6 +17,7 @@ app = FastAPI(
     title="Bookly",
     description="A REST API for a book review web service",
     version= version,
+    lifespan=life_span
 )
 
 app.include_router(book_router, prefix=f"/api/{version}/books", tags=['books'])
